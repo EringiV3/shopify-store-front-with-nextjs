b2588a9808744575901ec2958d3c1f4c40085846
萩原孝大 <kodaihagihara@EringiV3.local>
2020-12-01 00:07:54 +0900

    header実装

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,21 +1,41 @@
 import Link from 'next/link';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+// import { cartItemQuantityState } from '@/recoil/atoms/cartItemQuantityState';
+// import { useRecoilState } from 'recoil';
+// import { useEffect } from 'react';
+// import { useCart } from '@/hooks/cart/useCart';
 
-const Header: React.FC = () => (
-  <header>
-    <nav>
+const Header: React.FC = () => {
+  return (
+    <header>
+      {/* <nav>
       <Link href="/">
-        <a>Home</a>
-      </Link>{' '}
-      |{' '}
-      <Link href="/items">
         <a>Items</a>
-      </Link>
+      </Link>{' '}
       |{' '}
       <Link href="/cart">
         <a>Cart</a>
       </Link>
-    </nav>
-  </header>
-);
+    </nav> */}
+      <AppBar position="static">
+        <Toolbar>
+          {/* <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton> */}
+          <Link href={'/'}>
+            <Typography variant="h6">EringiV3 DemoStore</Typography>
+          </Link>
+
+          <Link href={'/cart'}>
+            <Button color="inherit">Cart(quantity: 0)</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </header>
+  );
+};
 
 export default Header;
